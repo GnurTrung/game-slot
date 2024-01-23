e1760c9e23f432d8f6872083e9b607ef83978d01
GnurT <trungvv@ftech.ai>
2024-01-23 15:33:15 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useWebApp } from './hooks';
 
 function App() {
+  const {WebApp} = useWebApp()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>{WebApp?.initDataUnsafe?.user?.username}x</code> and save to reload.
         </p>
         <a
           className="App-link"
--- src/hooks/useWebApp.ts
+++ src/hooks/useWebApp.ts
@@ -2,14 +2,15 @@ import { useEffect } from "react";
 import useMounted from "./useMounted";
 
 const useWebApp = () => {
-    const WebApp = typeof window !== 'undefined' && (window as any)?.Telegram?.WebApp
-        ? (window as any).Telegram.WebApp
-        : null;
-    const { isMounted } = useMounted();
-    useEffect(() => {
-        WebApp.ready();
-    }, [isMounted])
-    return WebApp;
-}
+  const WebApp =
+    typeof window !== "undefined" && (window as any)?.Telegram?.WebApp
+      ? (window as any).Telegram.WebApp
+      : null;
+  const { isMounted } = useMounted();
+  useEffect(() => {
+    if (isMounted && WebApp) WebApp.ready();
+  }, [isMounted,WebApp]);
+  return WebApp;
+};
 
 export default useWebApp;
